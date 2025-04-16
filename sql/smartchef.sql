5427fb958978943ffe04459f50d99080b2df5a1d
malekfhima <malekfhima1@gmail.com>
2025-04-16 14:01:30 +0100

    malek

--- sql/smartchef.sql
+++ sql/smartchef.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2025 at 09:51 AM
+-- Generation Time: Apr 16, 2025 at 02:15 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `smartchef`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recetts`
+--
+
+CREATE TABLE `recetts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `ingredients` text NOT NULL,
+  `preparation` text NOT NULL,
+  `preparation_time` int(11) NOT NULL,
+  `cooking_time` int(11) NOT NULL,
+  `servings` int(11) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `recetts`
+--
+
+INSERT INTO `recetts` (`id`, `name`, `ingredients`, `preparation`, `preparation_time`, `cooking_time`, `servings`, `image_path`, `user_id`, `created_at`) VALUES
+(5, 'fa9ous', '1kg fa9ous', 'tlifha bi sac w thotha fi tizik ', 25, 1, 1, 'uploads/67ff76fa06f14_Capture d\'écran 2025-04-02 002724.png', 2, '2025-04-16 09:23:06'),
+(7, 'makarouna', 'djej\r\nbsal\r\nmii\r\n', 'fgkklfgikfd:;llimbkj;', 22, 2, 4, 'uploads/67ff85979f735_Capture d\'écran 2025-04-11 104221.png', 2, '2025-04-16 10:25:27');
 
 -- --------------------------------------------------------
 
@@ -39,16 +66,20 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nom`, `mail`, `pass`) VALUES
-(1, 'MALEKFHIMA', 'malekfhima1@gmail.com', 'azertyuiop'),
-(2, 'aa', 'aa@gmail.com', 'azertyuiop'),
-(3, 'f', 'f@gmail.com', 'azertyuiop'),
-(4, 'MALEKF', 'malzfer@gmail.com', 'aze'),
-(5, 'kjlhgf', 'kgfd@gmail.com', 'aaa'),
-(6, 'aaaa', 'aaaa@gmail.com', 'aaa');
+(2, 'MALEKFHIMA', 'malekfhima1@gmail.com', '$2y$10$lni8rgh9YaJVPWCxi7UKuOiGu6YXypKF094qc3/0dqM41v2SnuyPK'),
+(3, 'mariem', 'mariem@gmail.com', '$2y$10$L7iD6dIvD6eHaAKP0enHfuEAA4bvFV2Ney7qVM0SoET1wEk16J0LG'),
+(4, 'mohamed', 'mohamed@gmail.com', '$2y$10$tK3uz6Pa4N/pli0DVi7eHuqiTkBGqLMoJKAvjtk4hIMZwOKHwWY6q');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `recetts`
+--
+ALTER TABLE `recetts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `user`
@@ -61,10 +92,26 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `recetts`
+--
+ALTER TABLE `recetts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `recetts`
+--
+ALTER TABLE `recetts`
+  ADD CONSTRAINT `recetts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
