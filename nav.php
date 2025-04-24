80100089e65f7370fcbc4c3f3125e46d0e705a7c
malekfhima <malekfhima1@gmail.com>
2025-04-24 12:09:57 +0100

    malek

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav class="main-navigation">
     <ul>
         <li><a href="index1.php">Accueil</a></li>
-        <li><a href="about.php">À propos</a></li>
+        <li><a href="about.html">À propos</a></li>
         <li><a href="contact.php">Contact</a></li>
         <li><a href="recettes.php">recetee</a></li>
         <li><a href="cat.php">cat</a></li>
