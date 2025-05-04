ef2d99ab5a661bdf603d209a7b7ad166afa0b40d
malek <malekfhim1@gmail.com>
2025-05-04 16:23:42 +0100

    malek

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
+
 <nav class="main-navigation">
     <ul>
         <li><a href="index1.php">Accueil</a></li>
         <li><a href="about.html">À propos</a></li>
         <li><a href="contact.php">Contact</a></li>
         <li><a href="recettes.php">recetee</a></li>
-        <li><a href="cat.php">cat</a></li>
     </ul>
 </nav>
